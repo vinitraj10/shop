40b6ab26f8862c763c2ccc7d81146597b5a11e72
vinitraj10 <vinitraj1012@gmail.com>
2018-10-06 18:09:09 +0530

    settings file updated

--- config/settings.py
+++ config/settings.py
@@ -108,3 +108,22 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+CORS_ORIGIN_ALLOW_ALL=True
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-disposition',
+    'content-type',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
+CORS_ORIGIN_WHITELIST = (
+    'google.com',
+    'localhost:8080'
+)
